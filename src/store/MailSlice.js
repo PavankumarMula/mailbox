c6dc5ab792fbfcf47153467cc7b5b2a4ff58c841
PavankumarMula <pavanpappy7@gmail.com>
2023-02-22 15:33:04 +0530

    sent functionality Added

--- src/store/MailSlice.js
+++ src/store/MailSlice.js
@@ -10,15 +10,15 @@ const mailSlice = createSlice({
   name: "mail",
   initialState: intialstate,
   reducers: {
-    addSentMails(state, action) {
-
-    },
+    addSentMails(state, action) {},
     onRefresh(state, action) {
-     const Allmails=action.payload
-     const toEmails=Allmails.filter(item=>item.to===state.email);
-     state.mailData=toEmails
-     const unreadmessagesArray=Allmails.filter(item=>item.read===false)
-     state.unreadmessages=unreadmessagesArray.length;
+      const Allmails = action.payload;
+      const toEmails = Allmails.filter((item) => item.to === state.email);
+      state.mailData = toEmails;
+      const unreadmessagesArray = Allmails.filter(
+        (item) => item.read === false
+      );
+      state.unreadmessages = unreadmessagesArray.length;
     },
     updated(state, action) {
       const mailId = action.payload.id;
@@ -31,24 +31,29 @@ const mailSlice = createSlice({
         0
       );
     },
-    deleteMail(state,action){
-      const presentmails=[...state.mailData]
-      const remainingMails=presentmails.filter(item=>item.id!==action.payload.id)
-      state.mailData=remainingMails;
+    deleteMail(state, action) {
+      const presentmails = [...state.mailData];
+      const remainingMails = presentmails.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.mailData = remainingMails;
     },
     setToEmail(state, action) {
       state.toEmail = action.payload;
     },
-    onLogin(state,action){
-      state.email=action.payload;
-      
+    sentMails(state, action) {
+       const sentMails=action.payload
+       const fromMyMails=sentMails.filter(item=>item.from===state.email)
+       state.mailData=fromMyMails
+    },
+    onLogin(state, action) {
+      state.email = action.payload;
     },
     onLogOut(state, action) {
       state.toEmail = null;
       state.email = null;
       state.mailData = []; // Reset mailData to empty array
-    }
-    
+    },
   },
 });
 
